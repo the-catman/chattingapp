d235c8cd9d066df9aaa92e9f47ca52353ff7e9df
the-catman <136928588+the-catman@users.noreply.github.com>
2026-01-01 18:46:07 +0200

    Update to E2EE, clean up the protocol

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 const WebSocket = require("ws");
-
-const http = require("http");
 const https = require("https");
 const { readFileSync } = require("fs");
-const crypto = require('crypto');
 
 const config = require("./config.json");
 
 function callBack(req, res) {
     res.writeHead(200);
-    res.end(readFileSync(config.client, { encoding: 'utf-8' }));
+    res.end(readFileSync(config.client, "utf-8"));
 }
 
-const server = config.useHTTPS ? (https.createServer({
+function debug(...args) {
+    if (config.logDebug) console.log("DEBUG::", ...args);
+}
+
+const server = https.createServer({
     key: readFileSync(config.keyDir),
     cert: readFileSync(config.certDir)
-}, callBack)) : (http.createServer(callBack));
+}, callBack);
 
 const wss = new WebSocket.Server({
     server,
     maxPayload: config.maxPayload
 });
 
-function debug(...args) {
-    if (config.logDebug) console.log("DEBUG::", ...args);
-}
-
-wss.on('connection', (ws, req) => {
+wss.on("connection", (ws, req) => {
     const ip = req.socket.remoteAddress;
     ws.ip = ip;
 
@@ -48,109 +45,78 @@ wss.on('connection', (ws, req) => {
 
     debug("Client connected.");
 
+    wss.clients.forEach((client) => {
+        if (client !== ws) {
+            client.send(JSON.stringify({
+                type: "anotherConnected"
+            }));
+        } else {
+            client.send(JSON.stringify({
+                type: "connectedUsers",
+                userAmount: wss.clients.size
+            }));
+        }
+    });
+
     ws.onclose = function (closeEvent) {
-        debug("Client disconnected, code: " + closeEvent.code, "reason: " + closeEvent.reason);
+        debug(`Client disconnected, code: ${closeEvent.code}, reason: ${closeEvent.reason}`);
 
         wss.clients.forEach((client) => {
             if (client !== ws) {
                 client.send(JSON.stringify({
-                    type: 'anotherDisconnected'
+                    type: "anotherDisconnected"
                 }));
             }
         });
     }
 
     ws.onmessage = function ({ data }) {
-        if (typeof data !== 'string') {
+        if (typeof data !== "string") {
             ws.close(1007, "Data must be of type string.");
             return;
         }
 
         let parsed;
+
         try {
             parsed = JSON.parse(String(data));
         } catch (err) {
             ws.close(1007, "Invalid JSON.");
             return;
         }
+
         switch (String(parsed.type)) {
-            case 'init':
+            case "message":
                 {
-                    const hashedAttempt = crypto.pbkdf2Sync(
-                        parsed.password,
-                        config.salt,
-                        config.iterations,
-                        config.keylen,
-                        config.digest
-                    ).toString('hex');
-
-                    if (ws.verified || hashedAttempt !== config.hashedPassword) {
-                        ws.close(1002, "Incorrect verification.");
-                        return;
-                    }
-
-                    debug("Client verified: " + ip);
-                    ws.verified = true;
-
-                    ws.send(JSON.stringify({
-                        type: 'accepted'
-                    }));
-
-                    wss.clients.forEach((client) => {
-                        if (client !== ws) {
-                            client.send(JSON.stringify({
-                                type: 'anotherConnected'
-                            }));
-                        } else {
-                            client.send(JSON.stringify({
-                                type: 'connectedUsers',
-                                userAmount: wss.clients.size
-                            }));
-                        }
+                    wss.clients.forEach(client => {
+                        client.send(JSON.stringify({
+                            type: "broadcast",
+                            content: parsed.content,
+                            iv: parsed.iv,
+                            you: client === ws
+                        }));
                     });
 
                     break;
                 }
-            case 'message':
+
+            case "file":
                 {
-                    const msg = String(parsed.content).trim().slice(0, 0xffff);
-
-                    if (msg !== "") {
-                        wss.clients.forEach((client) => {
-                            if (client.verified) {
-                                client.send(JSON.stringify({
-                                    type: 'broadcast',
-                                    content: msg,
-                                    you: client === ws
-                                }));
-                            }
-                        });
-                    }
-
+                    wss.clients.forEach(client => {
+                        if (client.readyState === WebSocket.OPEN) {
+                            client.send(JSON.stringify({
+                                type: "file",
+                                content: parsed.content,
+                                iv: parsed.iv,
+                                fileName: parsed.fileName,
+                                you: client === ws,
+                                isImg: parsed.isImg
+                            }));
+                        }
+                    });
                     break;
                 }
-            case 'file':
-                {
-                    parsed.content = String(parsed.content);
-                    parsed.isImg = Boolean(parsed.isImg);
-                    parsed.fileName = String(parsed.fileName);
 
-                    if (parsed.content && (parsed.isImg || parsed.fileName)) {
-                        wss.clients.forEach((client) => {
-                            if (client.verified) {
-                                client.send(JSON.stringify({
-                                    type: 'file',
-                                    content: parsed.content,
-                                    fileName: parsed.fileName,
-                                    you: client === ws,
-                                    isImg: parsed.isImg
-                                }));
-                            }
-                        });
-                    }
-
-                    break;
-                }
             default:
                 {
                     ws.close(1002, `Unknown packet type: \`${parsed.type}\`.`);
